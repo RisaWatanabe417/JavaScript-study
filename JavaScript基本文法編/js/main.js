7d432602f0e7755dbd1479081eab28d64ef085fe
RisaWatanabe417 <aquarius19840122@gmail.com>
2020-01-27 17:16:37 +0900

    JavaScript基本文法編#11

--- JavaScript基本文法編/js/main.js
+++ JavaScript基本文法編/js/main.js
@@ -1,21 +1,9 @@
 'use strict';
 
-// 変数 let var
+console.log('5' * 3);
+console.log('5' - '3');
 
-let price = 500;
+console.log('5' + 3);
 
-// price = price + 100;
-price += 100; // 600
-
-// price = price * 2;
-price *= 2; // 1200
-
-// price = price + 1;
-// price += 1;
-price++; // 1201
-
-// price = price - 1;
-// price-= 1;
-price--; // 1200
-
-console.log(price);
+console.log(parseInt('5', 10) +3);
+console.log(parseInt('hello', 10));
